46d8b52d12875bc3d199c15516ad6e7c700d64b1
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-12 02:09:45 +0100

    update

--- db/conn.php
+++ db/conn.php
@@ -3,7 +3,14 @@
     $db = 'attendance.db';
     $user = 'root';
     $pass = '';
+    $charset = 'utf8mb4';
 
+    $dsn = "mysql:host=$host;dbname:$db;charset=$charset";
 
+    try{
+        $pdo = new PDO($dsn,$user,$pass);
+    }catch(PDOExceptions $e){
+        throw new PDOException($e->getMessage());
+    }
 
 ?>
