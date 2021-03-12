3e43ace607c17df579b9ab762be1776863f7f308
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-12 13:36:32 +0100

    update

--- db/conn.php
+++ db/conn.php
@@ -1,11 +1,11 @@
 <?php
     $host = '127.0.0.1';
-    $db = 'attendance.db';
+    $db = 'attendance';
     $user = 'root';
     $pass = '';
     $charset = 'utf8mb4';
 
-    $dsn = "mysql:host=$host;dbname:$db;charset=$charset";
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
     try{
         $pdo = new PDO($dsn,$user,$pass);
