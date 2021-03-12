11f7e723e15f6307b45710dbfc071e04b893a0d6
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-12 01:46:52 +0100

    update

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" 
     crossorigin="anonymous">
+
+
     <link rel="stylesheet" href="css/site.css">
 
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
