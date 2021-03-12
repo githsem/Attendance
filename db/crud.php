4bb3bc456d31f4f8701478ccf74335cae9d586fb
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-12 03:22:07 +0100

    update

--- db/crud.php
+++ db/crud.php
@@ -9,17 +9,19 @@
 
         public function insert($fname, $lname, $dob, $email, $contact, $specialty){
             try {
-                $sql = "INSERT INTO attendee (firstname,lastname,dateofbirth,emailaddress,contactnumber,specialty_id) VALUES(:fname,:lname,:dob,:email,:contact,:specialty)";
+                $sql = "INSERT INTO attendee (firstname,lastname,dateofbirth,emailaddress,contactnumber,specialty_id) VALUES (:fname,:lname,:dob,:email,:contact,:specialty)";
                 $stmt = $this->db->prepare($sql);
 
                 $stmt->bindparam(':fname',$fname);
                 $stmt->bindparam(':lname',$lname);
                 $stmt->bindparam(':dob',$dob);
+                $stmt->bindparam(':email',$email);
                 $stmt->bindparam(':contact',$contact);
-                $stmt->bindparam(':specialty',$specialt);
+                $stmt->bindparam(':specialty',$specialty);
 
                 $stmt->execute();
                 return true;
+                
             } catch (PDOException $e) {
                 echo $e->getMessage();
                 return false;
