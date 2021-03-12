b8ee4fe04c47ef050ab8210624d9359d64a87aaf
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-12 01:46:41 +0100

    update

--- index.php
+++ index.php
@@ -6,39 +6,42 @@
     <h1 class="text-center">Registration for IT Conference</h1>
 
     
-    <form>
+    <form method = "post" action="success.php">
         <div class="mb-3">
             <label for="firstname" class="form-label">First Name</label>
-            <input type="text" class="form-control" id="firstname">        
+            <input type="text" class="form-control" id="firstname" name="firstname">        
         </div>
         <div class="mb-3">
             <label for="lastname" class="form-label">Last Name</label>
-            <input type="text" class="form-control" id="lastname">        
+            <input type="text" class="form-control" id="lastname" name="lastname">        
         </div>
         <div class="mb-3">
             <label for="dob" class="form-label">Date Of Birth</label>
-            <input type="text" class="form-control" id="dob">        
+            <input type="date" class="form-control" id="dob" name="dob">        
         </div>
         <div class="mb-3">
             <label for="specialty" class="form-label">Area of Expertise</label>
-            <select class="form-select" id="specialty">
-                <option value="1">Database Admin </option>
-                <option value="2">Software Developer</option>
-                <option value="3">Web Administrator</option>
-                <option value="3">Other</option>
+            <select class="form-select" id="specialty" name="specialty">
+                <option >Database Admin </option>
+                <option >Software Developer</option>
+                <option >Web Administrator</option>
+                <option >Other</option>
             </select>       
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email address</label>
-            <input type="text" class="form-control" id="phone" aria-describedby="emailHelp">
+            <input type="text" class="form-control" id="email" name="email" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
             <label for="phone" class="form-label">Contact Number</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="phoneHelp">
+            <input type="text" class="form-control" id="phone" name="phone" aria-describedby="phoneHelp">
             <div id="phone" class="form-text">We'll never share your number with anyone else.</div>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>  
+ 
+        <div class="d-grid gap-2">
+            <button class="btn btn-primary" type="submit" name="submit">Submit</button>
+        </div>
     </form>
 
     
