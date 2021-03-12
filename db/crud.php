a2aca4e2d5596b6eba1f9081eae04177a0c34046
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-12 02:39:09 +0100

    update

---
+++ db/crud.php
@@ -0,0 +1,30 @@
+<?php
+
+    class crud{
+        private $db;
+
+        function __construct($conn){
+            $this->db = $conn;
+        }
+
+        public function insert($fname, $lname, $dob, $email, $contact, $specialty){
+            try {
+                $sql = "INSERT INTO attendee VALUES(:fname,:lname,:dob,:email,:contact,:specialty)";
+                $stmt = $this->db->prepare($sql);
+
+                $stmt->bindparam(':fname',$fname);
+                $stmt->bindparam(':lname',$lname);
+                $stmt->bindparam(':dob',$dob);
+                $stmt->bindparam(':contact',$contact);
+                $stmt->bindparam(':specialty',$specialt);
+
+                $stmt->execute();
+                return true;
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                return false;
+            }
+        }
+    }
+
+?>
