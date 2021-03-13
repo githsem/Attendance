afb74a282f89e61304b14659a84fd558e009170a
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-13 18:39:23 +0100

    update

--- db/crud.php
+++ db/crud.php
@@ -7,7 +7,7 @@
             $this->db = $conn;
         }
 
-        public function insert($fname, $lname, $dob, $email, $contact, $specialty){
+        public function insertAttendees($fname, $lname, $dob, $email, $contact, $specialty){
             try {
                 $sql = "INSERT INTO attendee (firstname,lastname,dateofbirth,emailaddress,contactnumber,specialty_id)  VALUES (:fname,:lname,:dob,:email,:contact,:specialty)";
                 $stmt = $this->db->prepare($sql);
@@ -26,6 +26,11 @@
                 echo $e->getMessage();
                 return false;
             }
+        }
+        public function getAttendees(){
+            $sql = "SELECT * FROM 'attendee'";
+            $result = $this->db->query($sql);
+            return $result;
         }
     }
 
