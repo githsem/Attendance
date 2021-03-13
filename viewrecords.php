72d3774f202972868c82ad5a31873098cbbbf383
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-13 18:39:19 +0100

    update

--- viewrecords.php
+++ viewrecords.php
@@ -3,8 +3,18 @@
 
     require_once "includes/header.php";
     require_once 'db/conn.php'; 
+
+    //Get all attendees
+    $results = $crud->getAttendees();
 ?>
 
+<table class="table">
+    <tr>
+        <th>#</th>
+        <th>First Name</th>
+    </tr>
+    
+</table>
 
 
 
