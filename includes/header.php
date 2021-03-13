c29b14d89f1b291388445eaf44d9d5da09f811bc
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-13 18:39:28 +0100

    update

--- includes/header.php
+++ includes/header.php
@@ -19,7 +19,7 @@
   </head>
   <body>
     <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container-fluid">
         <a class="navbar-brand" href="index.php">IT Conference</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -31,7 +31,7 @@
               <a class="nav-link active" aria-current="page" href="index.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="viewrecords.php">View Attendies</a>
+              <a class="nav-link" href="viewrecords.php">View Attendees</a>
             </li>
           </ul>
         </div>
