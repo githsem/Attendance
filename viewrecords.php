366e2cc75d1673dd2eae230d2c3edf471b2d0937
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-13 20:00:36 +0100

    update

--- viewrecords.php
+++ viewrecords.php
@@ -12,7 +12,24 @@
     <tr>
         <th>#</th>
         <th>First Name</th>
+        <th>Last Name</th>
+        <th>Date of Birth</th>
+        <th>Email Address</th>
+        <th>Contact Number</th>
+        <th>Specialty</th>
     </tr>
+    <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) {?>
+        <tr>
+            <td><?php  echo $r['attendee_id']?></td>
+            <td><?php  echo $r['firstname']?></td>
+            <td><?php  echo $r['lastname']?></td>
+            <td><?php  echo $r['dateofbirth']?></td>
+            <td><?php  echo $r['emailaddress']?></td>
+            <td><?php  echo $r['contactnumber']?></td>
+            <td><?php  echo $r['specialty_id']?></td>
+        </tr>
+
+    <?php } ?>
     
 </table>
 
