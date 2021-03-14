e7d42d417ea8c76cee2c5119689119d77e4de61a
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-15 00:23:52 +0100

    update

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
     <form method = "post" action="success.php">
         <div class="mb-3">
             <label for="firstname" class="form-label">First Name</label>
-            <input type="text" class="form-control" id="firstname" name="firstname">        
+            <input required type="text" class="form-control" id="firstname" name="firstname">        
         </div>
         <div class="mb-3">
             <label for="lastname" class="form-label">Last Name</label>
-            <input type="text" class="form-control" id="lastname" name="lastname">        
+            <input required type="text" class="form-control" id="lastname" name="lastname">        
         </div>
         <div class="mb-3">
             <label for="dob" class="form-label">Date Of Birth</label>
@@ -35,7 +35,7 @@
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email address</label>
-            <input type="text" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+            <input required type="text" class="form-control" id="email" name="email" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
