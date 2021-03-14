c586c16b8b7ca457d6ce58b88386ee19dfa6ea1a
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-14 12:41:13 +0100

    update

--- edit.php
+++ edit.php
@@ -18,7 +18,7 @@
 
     
     <form method = "post" action="editpost.php">
-    <input type="hidden" name="id" value = "<?php echo $attendee['attendee_id'] ?>" />
+        <input type="hidden" name="id" value = "<?php echo $attendee['attendee_id'] ?>" />
         <div class="mb-3">
             <label for="firstname" class="form-label">First Name</label>
             <input type="text" class="form-control" value="<?php echo $attendee['firstname'] ?>" id="firstname" name="firstname">        
