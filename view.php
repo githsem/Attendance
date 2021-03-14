3ef1336e5b444a2c59b869c0eea4846b1e62b81f
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-14 01:34:46 +0100

    update

--- view.php
+++ view.php
@@ -7,6 +7,25 @@
     //Get all attendees
     
 ?>
+<div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">
+                <?php echo $_POST['firstname'] . ' ' .$_POST['lastname'];  ?>
+            </h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+                <?php echo $_POST['specialty'];  ?>
+            </h6>
+            <p class="card-text">
+                Date Of Birth : <?php echo $_POST['dob'];  ?>
+            </p>
+            <p class="card-text">
+                Email Adress : <?php echo $_POST['email'];  ?>
+            </p>
+            <p class="card-text">
+                Contact Number : <?php echo $_POST['phone'];  ?>
+            </p>
+        </div>
+    </div>    
 
 
 <?php require_once "includes/footer.php"; ?>  
