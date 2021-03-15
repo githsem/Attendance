1c8ea395410f11d8e07fbaf58baa5f8aad1984c0
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-15 23:35:50 +0100

    update

--- db/user.php
+++ db/user.php
@@ -7,5 +7,29 @@
         function __construct($conn){
             $this->db = $conn;
         }
+
+        public function insertUser($username, $password){
+            try {
+                $sql = "INSERT INTO users (username,password)  VALUES (:username,:password)";
+               
+                $stmt = $this->db->prepare($sql);
+
+                $stmt->bindparam(':usernam',$username);
+                $stmt->bindparam(':password',$password);
+                
+                $stmt->execute();
+                return true;
+
+            } catch (PDOException $e) {
+                echo $e->getMessage($username, $password);
+                return false;
+            }
+        
+
+        }
+
+        public function getUser(){
+
+        }
     }
 ?>        
