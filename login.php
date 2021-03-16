53ae55e45628020fc77deee170617de30c458e32
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-16 22:59:23 +0100

    update

--- login.php
+++ login.php
@@ -6,4 +6,17 @@
 
 ?>
 <h1 class="text-center"><?php echo $title ?></h1>
-<form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post"></form>
+<form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+    <table class="table table-sm">
+    <tr>
+        <td><label for = "username"> Username: * </label></td>
+        <td><input type="text" name="username" class="form-contrrol" id="username" value="<?php if($_SERVER['REQUEST_METHOD']=='POST') echo $_POST['username']; ?>"> </td>
+    </tr>
+    <tr>
+        <td><label for = "password"> Password: * </label></td>
+        <td><input type="password" name="password" class="form-control" id="password"> </td>
+    </tr>
+    </table><br><br>
+    <input type="submit" value="login" class = "btn btn-primary btn-block"><br>
+    <a href="#">Forgot Password</a>
+</form>
