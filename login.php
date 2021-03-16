b71e4cccd541f932fae0963857d08b65321b075b
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-16 23:10:07 +0100

    update

--- login.php
+++ login.php
@@ -4,13 +4,17 @@
     require_once "includes/header.php";
     require_once 'db/conn.php'; 
 
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        $username= strtolower(trim($_POST['username']));
+    }
+
 ?>
 <h1 class="text-center"><?php echo $title ?></h1>
 <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
     <table class="table table-sm">
     <tr>
         <td><label for = "username"> Username: * </label></td>
-        <td><input type="text" name="username" class="form-contrrol" id="username" value="<?php if($_SERVER['REQUEST_METHOD']=='POST') echo $_POST['username']; ?>"> </td>
+        <td><input type="text" name="username" class="form-control" id="username" value="<?php if($_SERVER['REQUEST_METHOD']=='POST') echo $_POST['username']; ?>"> </td>
     </tr>
     <tr>
         <td><label for = "password"> Password: * </label></td>
