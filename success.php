0d08abb832e5036fb37fcbb9fd0c532bed8c2d0d
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-18 21:31:09 +0100

    update

--- success.php
+++ success.php
@@ -11,13 +11,16 @@
         $contact = $_POST['phone'];
         $specialty = $_POST['specialty'];
 
-        $orig_file = $_FILES["avatar"]["tmp_name"];
-        $ext = pathinfo($_FILES["avatar"]["name"],PATHINFO_EXTENSION);
-        $target_dir = 'uploads/';
-        $destination = "$target_dir$contact.$ext"; 
-        move_uploaded_file($orig_file,$destination);
+        if(isset($_FILES["avatar"]["tmp_name"])){
+             $orig_file = $_FILES["avatar"]["tmp_name"];
+            $ext = pathinfo($_FILES["avatar"]["name"],PATHINFO_EXTENSION);
+            $target_dir = 'uploads/';
+            $destination = "$target_dir$contact.$ext"; 
+            move_uploaded_file($orig_file,$destination);
+        }
 
-        exit();
+       
+
 
         $isSuccess = $crud->insertAttendees($fname,$lname,$dob,$email,$contact,$specialty);
 
