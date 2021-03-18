e9fe063b9539844cc18f232b9d9e912819534ca6
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-03-18 21:20:49 +0100

    update

--- success.php
+++ success.php
@@ -12,7 +12,7 @@
         $specialty = $_POST['specialty'];
 
         $orig_file = $_FILES["avatar"]["tmp_name"];
-        $ext = pathinfo($_FILES["avatar"]["tmp_name"],PATHINFO_EXTENSION);
+        $ext = pathinfo($_FILES["avatar"]["name"],PATHINFO_EXTENSION);
         $target_dir = 'uploads/';
         $destination = "$target_dir$contact.$ext"; 
         move_uploaded_file($orig_file,$destination);
